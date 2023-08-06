ed673af27a63e2c43a0fb2173343251b8ef371b0
mundotv789123 <789michaelfernandes789@gmail.com>
2023-08-06 11:56:55 -0300

    Ajustes simples e limpeza

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,7 @@ export interface Message {
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 
 export class AppComponent implements OnInit {
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -4,7 +4,6 @@ import { Message } from '../app.component';
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
 })
 export class MessageComponent {
   @Input() message!: Message;
