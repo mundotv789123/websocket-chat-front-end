6e5ec1912c6216e588da635d6ee99e1fc06354f6
mundotv789123 <789michaelfernandes789@gmail.com>
2024-06-04 08:03:18 -0300

    configurando PWA

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    apiUrl: "http://localhost:8080",
-    websocketUrl: "ws://localhost:8080/ws",
+    apiUrl: "http://192.168.0.165:8080",
+    websocketUrl: "ws://192.168.0.165:8080/ws",
     websocketTopicMessageChannel: "/topic/chat",
     websocketAppMessageChannel: "/app/message"
 };
